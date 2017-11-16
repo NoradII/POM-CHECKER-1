1ea4c73a59bae9cea75a416a1ac823cc55367ed9
IamSSam <kongse92@gmail.com>
2017-11-16 15:44:07 +0900

    Add Progreesbar

--- public_html/C2M/c2m.js
+++ public_html/C2M/c2m.js
@@ -1,8 +1,73 @@
+
+
 window.onload = function() {
-  var ct_progress_bar = document.getElementById("ct-progress-bar");
-  var mri_progress_bar = document.getElementById("mri-progress-bar");  
-  move(ct_progress_bar, 20);
-  move(mri_progress_bar, 80);
+  var ctProgressbar = new ProgressBar.Circle('#ct-progress-bar', {
+      color: '#EB5244',
+      trailColor: '#151D23',
+      duration: 1500,
+      easing: 'easeInOut',
+      strokeWidth: 20,
+      text: {
+        className: 'progressbar_ct_label',
+        autoStyleContainer: true
+      },
+      step: function(state, circle) {
+        var value = Math.round(circle.value() * 100);
+        if(value === 0) {
+          circle.setText('');
+        }else {
+          circle.setText(value);
+        }
+      },
+      warnings: false
+  });
+  
+  var ctProgressBarContainer = document.getElementById('ct-progress-bar');
+  var ctProgressBarText = document.createElement('div');
+  ctProgressBarText.setAttribute("id", "ct-progress-bar-text");
+  ctProgressBarText.innerHTML = "CT"
+  ctProgressBarContainer.appendChild(ctProgressBarText);
+  
+  ctProgressbar.text.style.fontSize= '1rem';
+  ctProgressbar.text.style.marginTop = '15px';
+  ctProgressbar.text.style.fontWeight = 'bold';
+  var ctPercent = 80
+  ctProgressbar.animate(ctPercent/100);
+
+  var mrProgressbar = new ProgressBar.Circle('#mr-progress-bar', {
+      color: '#EB5244',
+      trailColor: '#151D23',
+      duration: 1500,
+      easing: 'easeInOut',
+      strokeWidth: 20,
+      text: {
+        className: 'progressbar_mr_label',
+        autoStyleContainer: true
+      },
+      step: function(state, circle) {
+                
+        var value = Math.round(circle.value() * 100);
+        if(value === 0) {
+          circle.setText('');
+        }else {
+          circle.setText(value);
+        }
+      },
+      warnings: false,
+      
+  });
+  
+  var mrProgressBarContainer = document.getElementById('mr-progress-bar');
+  var mrProgressBarText = document.createElement('div');
+  mrProgressBarText.setAttribute("id", "mr-progress-bar-text");
+  mrProgressBarText.innerHTML = "MRI"
+  mrProgressBarContainer.appendChild(mrProgressBarText);
+  
+  var mrPercent = 40;
+  mrProgressbar.text.style.fontSize= '1rem';
+  mrProgressbar.text.style.fontWeight = 'bold';
+  mrProgressbar.text.style.marginTop = '15px';
+  mrProgressbar.animate(mrPercent/100);
 
   document.getElementById("ct-image").style.height = document.getElementById("ct-image").offsetWidth;
   document.getElementById("generated-mri-image").style.height = document.getElementById("generated-mri-image").offsetWidth;
